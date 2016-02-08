5f7708457f3dfeb4b272b5a47109ff42fd585646
Sathwik <sathwik.samineni@gmail.com>
2016-02-08 11:02:13 -0500

    Initial Commit

---
+++ Demo_git/src/org/avani/myproject/mainclass/mymain.java
@@ -0,0 +1,10 @@
+package org.avani.myproject.mainclass;
+
+public class mymain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		System.out.println("Hello Worlds");
+	}
+
+}
